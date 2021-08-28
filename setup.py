4c46f143f91df2c173f80d2a1078bc866c0c202f
a1025 <a1025952421@outlook.com>
2021-08-28 17:42:40 +0800

    change: update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='Elaina',
-    version='0.0.1',
-    packages=['ela', 'ela.event', 'ela.message', 'ela.component', 'command', 'img_search'],
+    version='0.1.0',
+    packages=['ela', 'ela.event', 'ela.message', 'ela.component'],
     url='https://github.com/wyapx/Elaina',
     license='LGPL v2.1',
     author='wyapx',
