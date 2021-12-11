fc84dfd02e1bb0c7e2605b15f255477e771ccfd9
a1025 <a1025952421@outlook.com>
2021-12-11 14:13:10 +0800

    update: setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.1',
     packages=['ela', 'ela.event', 'ela.message', 'ela.component'],
     url='https://github.com/wyapx/Elaina',
-    license='LGPL v2.1',
+    license='AGPL v3',
     author='wyapx',
     author_email='null@teahou.se',
     description='一个高性能的mirai sdk（基于mirai-api-http）'
